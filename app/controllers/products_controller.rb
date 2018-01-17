8fa8346791e1d003b02406f8ae065d4ae3426b18
matt6390 <matthewstone639@gmail.com>
2018-01-16 19:59:36 -0600

    The app now has method for all of them, plus each individual

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     products = Product.all
     render json: products.as_json
   end
+
+  def teddy_method
+    product = Product.find_by(name: "One-Eyed Teddy")
+    render json: product.as_json
+  end
+
+  def calc_method
+    product = Product.find_by(name: "Constant Calculator")
+    render json: product.as_json
+  end
+
+  def capstone_method
+    product = Product.find_by(name: "Capstone Cookbook")
+    render json: product.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "/all_available_products" => 'products#all_available_method'
+  get "/teddy_for_sale" => 'products#teddy_method'
+  get "/worthless_calc_for_sale" => 'products#calc_method'
+  get "/capstone_for_sale" => 'products#capstone_method'
 end
