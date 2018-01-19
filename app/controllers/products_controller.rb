ae9805b2cbb9dd537c45e00da033e2846cc46b6b
matt6390 <matthewstone639@gmail.com>
2018-01-18 19:07:16 -0600

    Changed to a new naming scheme

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,21 +1,8 @@
 class ProductsController < ApplicationController
-  def all_available_method
+
+  def index
     products = Product.all
     render json: products.as_json
   end
-
-  def teddy_method
-    product = Product.find_by(name: "One-Eyed Teddy")
-    render json: product.as_json
-  end
-
-  def calc_method
-    product = Product.find_by(name: "Constant Calculator")
-    render json: product.as_json
-  end
-
-  def capstone_method
-    product = Product.find_by(name: "Capstone Cookbook")
-    render json: product.as_json
-  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,3 @@
 Rails.application.routes.draw do
-  get "/all_available_products" => 'products#all_available_method'
-  get "/teddy_for_sale" => 'products#teddy_method'
-  get "/worthless_calc_for_sale" => 'products#calc_method'
-  get "/capstone_for_sale" => 'products#capstone_method'
+  get "/products" => 'products#index'
 end
