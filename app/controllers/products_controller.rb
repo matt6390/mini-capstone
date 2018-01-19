65bd58fc46edfee3186c653e5fb501af990ae59d
matt6390 <matthewstone639@gmail.com>
2018-01-18 21:29:51 -0600

    The app now can index, create, show, update, and destroy entries

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -21,4 +21,33 @@ class ProductsController < ApplicationController
     render json: product.as_json
   end
 
+  def update
+    product = Product.find(params[:id])
+
+    product.name = params[:name]
+    product.price = params[:price]
+    product.image_url = params[:image_url]
+    product.description = params[:description]
+    product.save
+
+    render json: product.as_json
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+
+    render json: {message: "Congrats, you have succesfully destroyed product ##{product.id}"}    
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/products" => 'products#index'
   post "/products" => 'products#create'
   get "/products/:id" => 'products#show'
+  patch "/products/:id" => 'products#update'
+  delete "products/:id" => 'products#destroy'
 end
