084da68aab855d4e106dac127d99161b9f493d26
matt6390 <matthewstone639@gmail.com>
2018-01-18 20:31:14 -0600

    Client is improved, with forms

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,17 @@ class ProductsController < ApplicationController
     render json: products.as_json
   end
 
+  def create
+    product = Product.new(
+                          name: params[:name],
+                          price: params[:price],
+                          image_url: params[:image_url],
+                          description: params[:description]
+                          )
+    product.save
+    render json: product.as_json
+  end
+
   def show
     product = Product.find(params[:id])
     render json: product.as_json
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/products" => 'products#index'
+  post "/products" => 'products#create'
   get "/products/:id" => 'products#show'
 end
