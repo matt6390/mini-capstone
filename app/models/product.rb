5d70c16bfabf3a13f0996fa035e7d19eb8386ca9
matt6390 <matthewstone639@gmail.com>
2018-01-21 11:19:19 -0600

    The code now has as_json written in the models file, as well as I added a few more methods to make it look more user-friendly

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,42 @@
 class Product < ApplicationRecord
+
+  def is_discounted
+    if price < 30 
+      true
+    else 
+      false
+    end 
+  end
+
+  def tax
+    price * 0.09
+  end
+
+  def total
+    tax + price
+  end
+
+  def friendly_created_at
+    created_at.strftime("%m-%e-%y, %H:%M")
+  end
+
+  def friendly_updated_at
+    updated_at.strftime("%m-%e-%y, %H:%M")
+  end
+
+  def as_json
+            {
+              id: id,
+              name: name,
+              price: price,
+              is_discounted: is_discounted,
+              tax: tax,
+              total: total,
+              image_url: image_url,
+              description: description,
+              created_at: friendly_created_at,
+              updated_at: friendly_updated_at
+            }
+    
+  end
 end
