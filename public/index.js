0113aae10ab12f2017bae3c26429390042a63e84
matt6390 <matthewstone639@gmail.com>
2018-02-20 20:11:47 -0600

    Finished setting it up with signup login, and logout for bootstrap/vue.js in mini capstone

--- public/index.js
+++ public/index.js
@@ -1,18 +1,123 @@
-var productTemplate = document.querySelector("#product-card");
-var productContainer = document.querySelector(".row");
-console.log(productContainer);
+/* global Vue, VueRouter, axios */
 
-axios.get("http://localhost:3000/products").then(function(response) {
-  var products = response.data;
-  products.forEach(function(product) {
-    var productClone = productTemplate.content.cloneNode(true);
-    productClone.querySelector(".title").innerText = product.name;
-    productClone.querySelector(".price").innerText = product.price;
-    productClone.querySelector(".description").innerText = product.description;
-    productClone.querySelector(".supplier-name").innerText =
-      product.supplier.name;
-    productClone.querySelector(".supplier-number").innerText =
-      product.supplier.phone_number;
-    productContainer.appendChild(productClone);
-  });
+var HomePage = {
+  template: "#home-page",
+  data: function() {
+    return {
+      message: "This is my Home Page!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var SamplePage = {
+  template: "#sample-page",
+  data: function() {
+    return {
+      message: "This is my Sample Page!"
+    };
+  },
+  created: function() {},
+  methods: {},
+  computed: {}
+};
+
+var SignupPage = {
+  template: "#signup-page",
+  data: function() {
+    return {
+      name: "",
+      email: "",
+      password: "",
+      passwordConfirmation: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        name: this.name,
+        email: this.email,
+        password: this.password,
+        password_confirmation: this.passwordConfirmation
+      };
+      axios
+        .post("/users", params)
+        .then(function(response) {
+          router.push("/login");
+        })
+        .catch(
+          function(error) {
+            this.errors = error.response.data.errors;
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LoginPage = {
+  template: "#login-page",
+  data: function() {
+    return {
+      email: "",
+      password: "",
+      errors: []
+    };
+  },
+  methods: {
+    submit: function() {
+      var params = {
+        auth: { email: this.email, password: this.password }
+      };
+      axios
+        .post("/user_token", params)
+        .then(function(response) {
+          axios.defaults.headers.common["Authorization"] =
+            "Bearer " + response.data.jwt;
+          localStorage.setItem("jwt", response.data.jwt);
+          router.push("/");
+        })
+        .catch(
+          function(error) {
+            this.errors = ["Invalid email or password."];
+            this.email = "";
+            this.password = "";
+          }.bind(this)
+        );
+    }
+  }
+};
+
+var LogoutPage = {
+  created: function() {
+    axios.defaults.headers.common["Authorization"] = undefined;
+    localStorage.removeItem("jwt");
+    router.push("/");
+  }
+};
+
+var router = new VueRouter({
+  routes: [
+    { path: "/", component: HomePage },
+    { path: "/sample", component: SamplePage },
+    { path: "/signup", component: SignupPage },
+    { path: "/login", component: LoginPage },
+    { path: "/logout", component: LogoutPage }
+  ],
+  scrollBehavior: function(to, from, savedPosition) {
+    return { x: 0, y: 0 };
+  }
+});
+
+var app = new Vue({
+  el: "#vue-app",
+  router: router,
+  created: function() {
+    var jwt = localStorage.getItem("jwt");
+    if (jwt) {
+      axios.defaults.headers.common["Authorization"] = jwt;
+    }
+  }
 });
